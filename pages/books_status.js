a89f1cdcf18be16b3ccd019ee8e92f4c8a029195
szeyic@gmail.com <szeyic@gmail.com>
2024-03-18 16:22:21 -0400

    class activities

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
 let BookInstance = require('../models/bookinstance');
 
+// return list of object {title,status} where the status is 'Available'
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  BookInstance.find({status: 'Available'})
+    .populate('book')
+    .exec()
+    .then(list_bookinstances => {
+      res.send(list_bookinstances.map(function(bookinstance){
+        return BookInstance.book.title + " : " + BookInstance.status;
+      }));
+    })
+    .catch(err => {res.send('Status not found ')});
 }
